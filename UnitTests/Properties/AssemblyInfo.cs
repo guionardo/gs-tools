02d3d20a6f97907bbe60863a6a605f656b1ed1a7
Guionardo Furlan <guionardo@gmail.com>
2018-08-23 08:32:14 -0300

    Log class

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77469f75-0765-4e81-b3d8-05877f8ee62e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GS.Tools;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestCategory("Log")]
+        [TestMethod]
+        public void TestLog_YearMonthWeek()
+        {
+            var l = new Log("teste.log",suffixLog:FileNameSuffixLog.YearMonthWeek,timestampType:TimeStampType.JustTimeMilliseconds);
+            l.Add("TESTE DE LINHA\nLinha2", "Grupo");
+        }
+        [TestCategory("Log")]
+        [TestMethod]
+        public void TestLog_YearWeek()
+        {
+            var l = new Log("teste.log", suffixLog: FileNameSuffixLog.YearWeek, timestampType: TimeStampType.JustTimeSeparatorMilliseconds);
+            l.Add("TESTE DE LINHA", "Grupo");
+        }
+        [TestCategory("Log")]
+        [TestMethod]
+        public void TestLog_YearMonth()
+        {
+            var l = new Log("teste.log", suffixLog: FileNameSuffixLog.YearMonth, timestampType: TimeStampType.DateTime);
+            l.Add("TESTE DE LINHA", "Grupo");
+        }
+        [TestCategory("Log")]
+        [TestMethod]
+        public void TestLog_Date()
+        {
+            var l = new Log("teste.log", suffixLog: FileNameSuffixLog.Date, timestampType: TimeStampType.DateTimeMilliseconds);
+            l.Add("TESTE DE LINHA", "Grupo");
+        }
+    }
+}
